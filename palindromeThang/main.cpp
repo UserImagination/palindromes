8391f8904ac452d885bfae674c277480b8ec1072
hawk2 <joehaven@gmail.com>
2024-03-02 11:04:23 -0800

    Initial Commit

---
+++ palindromeThang/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  palindromeThang
+//
+//  Created by Joseph Craig on 3/2/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
